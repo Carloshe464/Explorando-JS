0036b83d0b811cad0b6e1871ef13199486c60af9
Carloshe464 <carloslemos464@gmail.com>
2024-01-17 12:25:31 -0300

    Atualização do codigo, sobre o retorno de lista

--- Fundamentos/variaveisEconstanste[minha_versao_teste_conhecimento_js].js
+++ Fundamentos/variaveisEconstanste[minha_versao_teste_conhecimento_js].js
@@ -4,9 +4,9 @@ let debora = 0
 console.log('Eu tenho apenas ',carlos ,'de experiencia no mercado cripto')
 
 //Redeclarando a variavel dentro do codigo.
-carlos = ['Funciona como uma lista? ']
+carlos = ['Funciona como uma lista? Não, o retorno é como objetc']
 
 //Chamando a nova variavel atualizada.
 console.log('Eu tenho apenas ',carlos ,'de experiencia no mercado cripto')
 
-console.log(carlos)
+console.log(typeof carlos)
