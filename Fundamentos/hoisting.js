48ded7ffb4207c64d47676745dad6596533e14ac
Carloshe464 <carloslemos464@gmail.com>
2024-01-18 22:26:39 -0300

    Atualização

---
+++ Fundamentos/hoisting.js
@@ -0,0 +1,6 @@
+console.log('a =', a)
+//var a = 2
+console.log('a =', a)
+
+//Caso a variavel n seja declarada em nenhum momento do codigo, da erro.
+//Caso seja declarada depois, temos o famoso hoisting (inçar pra cima) somente com var isso ocorre.
---
+++ Fundamentos/par_valor.js
@@ -0,0 +1,25 @@
+const saudacao = 'opa' // contexto logico 1
+
+{
+function aprendi (){
+    let saudacao = 'Fallllaa brodi'
+    console.log(saudacao)
+    }
+}
+
+const aprendi_mesmo = {
+    nome: "Carlos",
+    cor:"Verde",
+    tamanho:"1.78",
+    saude: {
+        atividades: "I'm playgin basketball",
+        lazer: "I love playing games"
+         },
+    status_civil:"Solteiro"
+}
+
+
+aprendi()
+console.log(saudacao)
+
+console.log(aprendi_mesmo(nome))
