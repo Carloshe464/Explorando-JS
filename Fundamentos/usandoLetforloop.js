d49daf194876f819dbfbd386e41009159c37b7a1
Carloshe464 <carloslemos464@gmail.com>
2024-01-18 21:31:48 -0300

    Atualização

---
+++ Fundamentos/usandoLetforloop.js
@@ -0,0 +1,6 @@
+for (let i = 0; i < 10; i++) { 
+    console.log('chegou ao limite!', i)
+}
+
+console.log('i = ', i)
+
---
+++ Fundamentos/usandoVaremloop2.js
@@ -0,0 +1,10 @@
+const funcs = []
+//i = 0
+for (i = 0 ; i < 10; i++) {
+    funcs.push(function (){
+    console.log(i)
+})}
+    
+
+
+funcs[2]()
