a65e3a14c0aae708e8e9aa4a82b843ff42e285fc
Carloshe464 <carloslemos464@gmail.com>
2024-01-18 21:20:12 -0300

    Atualização

--- Fundamentos/Teste_nivelamento.js
+++ Fundamentos/Teste_nivelamento.js
@@ -10,7 +10,7 @@
 }
 }
 
-rl.question('Qual moeda, você comprou? ', (resposta) => {console.log('Então você tem a moeda $[resposta]')})
+rl.question ('Qual moeda, você comprou? ', (resposta) => {console.log('Então você tem a moeda $[resposta]')})
 
 /*preco = 1
 valor = 20
@@ -19,4 +19,12 @@ total = preco * valor
 
 //console.log(preco)
 //console.log("Agora eu tenho, um total de :", total)
+*/
+
+/*
+*
+*Travei aqui, por não conhecer bem a entrada de dados
+*
+*
+*
 */
